3793353e706b58e3301a85b00318a5101ce3db09
Bassirou2004 <bassmboup2004@gmail.com>
2026-06-03 22:04:18 +0000

    docs: ajout du guide utilisateur de l'application de gestion scolaire

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User, Group
 
 
 def user_login(request):
@@ -21,3 +22,34 @@ def user_logout(request):
     logout(request)
     messages.success(request, 'Déconnexion réussie.')
     return redirect('login')
+
+
+
+from django.contrib.auth.models import User, Group
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        role = request.POST['role']
+
+        if password1 != password2:
+            return render(request, 'accounts/register.html', {
+                'error': 'Les mots de passe ne correspondent pas.'
+            })
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'accounts/register.html', {
+                'error': 'Ce nom d\'utilisateur existe déjà.'
+            })
+
+        user = User.objects.create_user(username=username, password=password1)
+        group = Group.objects.get(name=role)
+        user.groups.add(group)
+        user.save()
+        login(request, user)
+        messages.success(request, f'Bienvenue {user.username} !')
+        return redirect('index')
+
+    return render(request, 'accounts/register.html')
